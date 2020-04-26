ac053916530562993960d35c51843d2b0bde0c0f
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-04-26 09:58:35 +0800

    feat(commit-screen): added basic tree for commit screen

---
+++ packages/desktop-app/src/CommitScreen/__tests__/index.ts
@@ -0,0 +1,5 @@
+import CommitScreen from "../";
+
+describe("@electron-git/desktop-app/CommitScreen", () => {
+  it("should allow to select which files to commit", async () => {});
+});
---
+++ packages/desktop-app/src/CommitScreen/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Tree from "rc-tree";
+import { v4 as uuid } from "uuid";
+
+import "./treeStyle.css";
+
+const forEachNodeInTree = (tree, mutationFunc) => {
+  return tree.map((node) => {
+    if (node.children) {
+      node.children = forEachNodeInTree(node.children, mutationFunc);
+    }
+
+    // if the mutation doesn't return anything then the mutation was internal
+    const mutationResult = mutationFunc(node);
+    return mutationResult ? mutationResult : node;
+  });
+};
+
+const prepareData = (tree) => {
+  return forEachNodeInTree(tree, (node) => {
+    node.toggled = true;
+    node.key = uuid();
+    node.title = node.name;
+    node.checkable = true;
+  });
+};
+
+export default ({ files }) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setData(prepareData(files));
+  }, [files]);
+
+  const onCheck = (checkedKeys, a) => {
+    console.log(checkedKeys, a);
+  };
+
+  return (
+    <div>
+      <div>Select the files you'd like to commit </div>
+      {data && (
+        <Tree checkable defaultExpandAll treeData={data} onCheck={onCheck} />
+      )}
+    </div>
+  );
+};
